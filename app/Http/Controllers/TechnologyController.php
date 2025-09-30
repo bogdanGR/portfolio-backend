fac3d2cf07de8ca93a4eb4ff5dc122c49ce532f5
bogdanGR <fant1k450@gmail.com>
2025-09-30 13:27:46 +0300

    Remove unsuded action show() from TechnologyController

--- app/Http/Controllers/TechnologyController.php
+++ app/Http/Controllers/TechnologyController.php
@@ -48,14 +48,6 @@ class TechnologyController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      */
     public function edit(Technology $technology)
