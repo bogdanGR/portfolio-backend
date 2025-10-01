f864ea12c2d15248df29ffbbc2bde9e36aaf4b13
bogdanGR <fant1k450@gmail.com>
2025-10-01 14:02:37 +0300

    Refactor Technology model

--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -3,17 +3,37 @@
 namespace App\Models;
 
 use App\Enums\TechnologyCategory;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Carbon;
 
+/**
+ * Technology Model
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $slug
+ * @property TechnologyCategory $category
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ *
+ * @property-read Collection<int, Project> $projects
+ */
 class Technology extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
     protected $fillable = ['name', 'slug', 'category'];
 
     /**
      * Returns Projects related to technology
-     * @return Project
+     * @return BelongsToMany
      */
-    public function projects(): Project
+    public function projects(): BelongsToMany
     {
         return $this->belongsToMany(Project::class)
             ->withPivot(['sort_order'])
